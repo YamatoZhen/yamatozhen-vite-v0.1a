ae98563cb95ad25dab14167dc19638280ef5e9c9
Yamato Zhen <101009612+YamatoZhen@users.noreply.github.com>
2025-06-28 21:35:46 +0300

    fixed an issue where having multiple buttons crashes the DOM

--- src/links/HomePage.tsx
+++ src/links/HomePage.tsx
@@ -1,14 +1,16 @@
+import Button from '../components/button/Button'
 import Carousel from '../components/carousel/Carousel';
 import Dropdown from '../components/dropdown/dropdown';
 import MIOCard from '../components/MIOCard/MIOCard';
 import PFP from '../components/pfp/PFP';
+import Sinewave from '../components/sinewave/Sinewave'
 import Thumbnail, { GridContainer } from '../components/thumbnail/Thumbnail';
+import Switch from '../components/switch/Switch';
 import FTB from '../components/FTB/FTB';
 import AnimateChildren from '../components/FramerMotion/PageWrapper';
 import ReviewForm from '../components/Reviews/ReviewForm';
 import Accordion from '../components/accordion/Accordion';
 import { Badge } from '../components/badge/Badge';
-import Sinewave from '../components/sinewave/Sinewave';
 
 const urls = [
   "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3142bdda-d51f-4527-b457-9eb705a5f125/dg4b27v-70a0e48e-b135-4068-ba7e-2b9b1943e9f6.jpg/v1/fill/w_1920,h_2400,q_75,strp/_zblxt9__by_lazyskel_ai_dg4b27v-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMxNDJiZGRhLWQ1MWYtNDUyNy1iNDU3LTllYjcwNWE1ZjEyNVwvZGc0YjI3di03MGEwZTQ4ZS1iMTM1LTQwNjgtYmE3ZS0yYjliMTk0M2U5ZjYuanBnIiwiaGVpZ2h0IjoiPD0yNDAwIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvMzE0MmJkZGEtZDUxZi00NTI3LWI0NTctOWViNzA1YTVmMTI1XC9sYXp5c2tlbC1haS00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.40ndgn6CS4nsb1yV0zM_yO9dhRnDYniDKYzZ1tCZAxc",
@@ -47,6 +49,14 @@ function HomePage() {
             <p>I also follow trends in web animation, creative coding, WebGL design, 3D interfaces, AI-assisted design, and accessibility-first web development.</p>
 
             <img src="hero-image.png" alt="UI/UX design, frontend code, digital art – Aedan's portfolio hero" />
+
+            <Switch id={'1233409'} />
+            <Button id={''} type={'filled'}>Get started</Button>
+            <Button id={''} type={'tonal'}>Get started</Button>
+            <Button id={''} type={'outlined'}>Get started</Button>
+            <Button id={''} type={'text'}>Get started</Button>
+            <Button id={''} type={'filled-alt'}>Get started</Button>
+            <Sinewave />
             <Dropdown
               items={[
                 { href: '#', label: 'item' },
@@ -55,7 +65,6 @@ function HomePage() {
                 { href: '#', label: 'item' }
               ]} label={'Drop?'} id={''} type={'filled'} />
             <PFP className='pfp' size='128px' src="https://www.artstation.com/yamato_zhen" />
-            <Sinewave/>
             <GridContainer setColumns={1} setRows={2}>
               <Thumbnail src="src/assets/aa88421a029b7b834038eea81d3a060a.jpg" alt="1" />
               <Thumbnail src="" alt="" />
